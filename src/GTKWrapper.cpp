cf044623a35e559229ae51db5f3bcde567bc2468
h0lger <audidave@gmail.com>
2014-12-28 23:07:36 +0100

    Some colors to the window

--- src/GTKWrapper.cpp
+++ src/GTKWrapper.cpp
@@ -4,11 +4,17 @@ static void do_drawing(cairo_t *cr);
 
 static void do_drawing(cairo_t *cr)
 {
-	cairo_set_source_rgb(cr, 0, 0, 0);
-	cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
-	cairo_set_font_size(cr, 40.0);
-	cairo_move_to(cr, 10.0, 50.0);
-	cairo_show_text(cr, "Hello David");
+	cairo_set_source_rgb(cr, 0, .555, .100);
+
+	cairo_rectangle(cr, 0, 0, CELL_WEIGHT, CELL_WEIGHT);
+	cairo_rectangle(cr, (CELL_WEIGHT + CELL_SPACING), 0, CELL_WEIGHT, CELL_WEIGHT);
+	cairo_stroke_preserve(cr);
+	cairo_fill(cr);
+	
+	// cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+	// cairo_set_font_size(cr, 40.0);
+	// cairo_move_to(cr, 10.0, 50.0);
+	// cairo_show_text(cr, "Hello David");
 
 }
 
@@ -35,8 +41,9 @@ void show_window(int argc, char *argv[])
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
 	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-	gtk_window_set_default_size(GTK_WINDOW(window), 400, 90);
-	gtk_window_set_title(GTK_WINDOW(window), "My GTK");
+	gtk_window_set_default_size(GTK_WINDOW(window), DEF_SCREEN_X, DEF_SCREEN_Y);
+	gtk_window_set_title(GTK_WINDOW(window), "KataGameOfLife");
+	gtk_widget_override_background_color(darea, GTK_STATE_FLAG_NORMAL, &color_dark);
 
 	gtk_widget_show_all(window);
 	gtk_main();
--- src/GTKWrapper.h
+++ src/GTKWrapper.h
@@ -2,6 +2,11 @@
 #define GTK_WRAPPER_H
 #include <cairo.h>
 #include <gtk/gtk.h>
+#define DEF_SCREEN_X 400
+#define DEF_SCREEN_Y 400
+#define CELL_WEIGHT 5
+#define CELL_SPACING 3
+static GdkRGBA color_dark = {.105, .105, .105, 1.0};
 
 //static void do_drawing(cairo_t *cr);
 
